744b20d976e9b2c98381d19706fcb073ce70005e
AKJ95 <andyykp@gmail.com>
2024-07-24 10:30:04 +0800

    Try loading weights trained with old TensorFlow into a model defined in new version.

--- src/test_tf_model_loading.py
+++ src/test_tf_model_loading.py
@@ -1,6 +1,10 @@
-from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense
 
 
 if __name__ == '__main__':
-    model_path = 'models/Classifiers/softmax.cui.h5'
-    model = load_model(model_path)
+    model = Sequential([
+        Dense(18426, activation='softmax', input_shape=(768,)),
+    ])
+    model.load_weights("models/Classifiers/softmax.cui.h5")
+
