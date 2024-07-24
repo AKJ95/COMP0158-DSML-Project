94fe0246783367f40ebefeafdc71e487eee994ec
AKJ95 <andyykp@gmail.com>
2024-07-24 16:08:47 +0800

    Try loading mapping.

--- src/test_tf_model_loading.py
+++ src/test_tf_model_loading.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     print('Loading precomputed ...')
     mapping = label_mapping = joblib.load(path_precomputed_dev_map)
     mapping = {i: l for l, i in mapping.items()}
-    X_train, y_train, train_label_mapping = load_precomputed_embeddings(path_precomputed_train_vecs, mm_ann, mapping)
+    X_train, y_train, train_label_mapping = load_precomputed_embeddings(path_precomputed_train_vecs, mm_ann)
     X_dev, y_dev, _ = load_precomputed_embeddings(path_precomputed_dev_vecs, mm_ann, train_label_mapping)
     X_train = torch.from_numpy(X_train).to(device)
     y_train = torch.from_numpy(np.array(y_train)).to(device)
