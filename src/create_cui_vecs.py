65396a17acaabdfafe0d4a7df55cd11949acf92f
AKJ95 <andyykp@gmail.com>
2024-07-26 20:59:08 +0800

    Test generating VSM with all concepts with definition.

--- src/create_cui_vecs.py
+++ src/create_cui_vecs.py
@@ -29,13 +29,11 @@ for cui_idx, cui in enumerate(umls_kb.get_all_cuis()):
 
     if umls_kb.umls_data[cui]["DEF"]:
         cui_aliases_vecs = []
-
         for alias in umls_kb.get_aliases(cui, include_name=True):
             alias_toks = [t.text.lower() for t in sci_nlp(alias)]
-            definition_toks = [t.text.lower() for t in sci_nlp(umls_kb.umls_data[cui]["DEF"][0])]
-            combined_vecs = toks2vecs(alias_toks + definition_toks, return_tokens=False)
-            alias_vecs = toks2vecs(combined_vecs, return_tokens=False)
-            alias_vec = np.array(alias_vecs)[:len(alias_toks)].mean(axis=0)
+            alias_vecs = toks2vecs(alias_toks, return_tokens=False)
+
+            alias_vec = np.array(alias_vecs).mean(axis=0)
             cui_aliases_vecs.append(alias_vec)
 
         cui_vecs[cui] = np.array(cui_aliases_vecs).mean(axis=0)
