8648873fb0891c4b1b79b2bbc9d0b571858ee834
AKJ95 <andyykp@gmail.com>
2024-07-26 19:00:00 +0800

    Test with alias VSM of all concepts with definitions.

--- src/eval_mm_docs.py
+++ src/eval_mm_docs.py
@@ -24,6 +24,7 @@ ngram_map_path = 'data/processed/umls.2024AA.active.st21pv.aliases.5toks.map'
 st_vsm_path = 'models/VSMs/mm_st21pv.sts_anns.scibert_scivocab_uncased.vecs'
 # cui_vsm_path = 'models/VSMs/mm_st21pv.cuis.scibert_scivocab_uncased.vecs'
 cui_vsm_path = 'data/processed/mm_st21pv.cuis.scibert_scivocab_uncased.vecs'
+cui_def_vsm_path = 'data/processed/umls.2024AA.active.st21pv.scibert_scivocab_uncased.cuis.vecs'
 cui_idx_path = 'models/VSMs/umls.2017AA.active.st21pv.scibert_scivocab_uncased.cuis.index'
 cui_lbs_path = 'models/VSMs/umls.2017AA.active.st21pv.scibert_scivocab_uncased.cuis.labels'
 
@@ -44,7 +45,7 @@ medlinker = MedLinker(medner, umls_kb)
 # medlinker.load_string_matcher(ngram_db_path, ngram_map_path)
 # medlinker.load_cui_softmax_pt()
 # medlinker.load_cui_FaissVSM(cui_idx_path, cui_lbs_path)
-medlinker.load_cui_VSM(cui_vsm_path)
+medlinker.load_cui_VSM(cui_def_vsm_path)
 
 
 # input('...')
