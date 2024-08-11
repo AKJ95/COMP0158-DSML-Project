dd64f9f802c6d098c2982a91bebad6780353b485
AKJ95 <andyykp@gmail.com>
2024-08-11 15:58:54 +0800

    Train x encoder.

---
+++ src/train_x_encoder.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+    x_encoder_vectors = np.load('data/processed/x_encoder_vectors.npy')
+    x_encoder_labels = np.load('data/processed/x_encoder_labels.npy')
+    print(x_encoder_vectors.shape)
+    print(x_encoder_labels.shape)
+    print(np.sum(x_encoder_labels))
