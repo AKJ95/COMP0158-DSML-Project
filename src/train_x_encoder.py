3f54cb520096d55bf4054ced20a9fa742b06bebc
AKJ95 <andyykp@gmail.com>
2024-08-11 19:02:48 +0800

    Train x encoder.

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -1,10 +1,38 @@
 import numpy as np
 
+import torch
+from torch import nn
+from torch.utils.data import Dataset
+
+
+class EncoderDataset(Dataset):
+    def __init__(self, vectors, labels):
+        self.vectors = vectors
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.vectors)
+
+    def __getitem__(self, idx):
+        return self.vectors[idx], self.labels[idx]
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(768, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 
 if __name__ == '__main__':
     x_encoder_vectors = np.load('data/processed/x_encoder_vectors.npy')
     x_encoder_labels = np.load('data/processed/x_encoder_labels.npy')
-    print(x_encoder_vectors.shape)
-    print(x_encoder_labels.shape)
-    print(x_encoder_labels[:10])
-    print(x_encoder_vectors[:5][:20])
+    # Convert numpy arrays to PyTorch tensors
+    x_encoder_vectors = torch.from_numpy(x_encoder_vectors)
+    x_encoder_labels = torch.from_numpy(x_encoder_labels)
+    dataset = EncoderDataset(x_encoder_vectors, x_encoder_labels)
