bd2985598d78d0a8ee65765fdcdf0dc2c0e5cc2f
AKJ95 <andyykp@gmail.com>
2024-08-19 11:08:28 +0100

    Generate dev vectors for reranker.

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
             if instance_counter < 10:
                 for j in range(vectors.size(0)):
                     if instance_counter < 10:
-                        print(f'Model outputs for instance {instance_counter + 1}: {torch.sigmoid(outputs[j].item())}; Actual label: {labels[j].item()}')
+                        print(f'Model outputs for instance {instance_counter + 1}: {torch.sigmoid(outputs[j]).item()}; Actual label: {labels[j].item()}')
                         instance_counter += 1
                     else:
                         break
