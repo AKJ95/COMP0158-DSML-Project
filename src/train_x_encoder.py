0763741568245302019e2faeae547a16cf5e7fb3
AKJ95 <andyykp@gmail.com>
2024-08-19 10:34:37 +0100

    Generate dev vectors for reranker.

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 
     # Define the loss function and the optimizer
     criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # Define the number of epochs
     n_epochs = 100
