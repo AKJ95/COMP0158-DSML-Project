532fe5020d2ee8864f627d5959cd143faba127fa
AKJ95 <andyykp@gmail.com>
2024-08-19 11:07:57 +0100

    Generate dev vectors for reranker.

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -27,7 +27,7 @@ class MLP(nn.Module):
             nn.Linear(768, 128),
             nn.ReLU(),
             nn.Linear(128, 1),
-            nn.Sigmoid()
+            # nn.Sigmoid()
         )
 
     def forward(self, x):
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     model.train()
 
     # Define the loss function and the optimizer
-    criterion = nn.BCELoss()
+    criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([4.0]).to(device))
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # Define the number of epochs
@@ -87,7 +87,7 @@ if __name__ == '__main__':
             if instance_counter < 10:
                 for j in range(vectors.size(0)):
                     if instance_counter < 10:
-                        print(f'Model outputs for instance {instance_counter + 1}: {outputs[j].item()}; Actual label: {labels[j].item()}')
+                        print(f'Model outputs for instance {instance_counter + 1}: {torch.sigmoid(outputs[j].item())}; Actual label: {labels[j].item()}')
                         instance_counter += 1
                     else:
                         break
