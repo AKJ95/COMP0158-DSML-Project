da644eea02d3cef0185c7e71f72a7201aa853367
AKJ95 <andyykp@gmail.com>
2024-08-21 09:21:10 +0100

    Generate dev vectors for reranker.

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -157,8 +157,8 @@ def valid(model, testing_loader, device, id2label) -> Tuple[list, list, list, li
                 else:
                     word_level_predictions.append(wp_preds[index][1])
                     word_level_targets.append(targets[index])
-            print(len(word_level_predictions))
-            print(len(word_level_targets))
+            print(word_level_predictions[:50])
+            print(word_level_targets[:50])
             nervaluate_labels.append([id2label[tag_id.item()] for tag_id in word_level_targets])
             nervaluate_preds.append([id2label[tag_id.item()] for tag_id in word_level_predictions])
 
