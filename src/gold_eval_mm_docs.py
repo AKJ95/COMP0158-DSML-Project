a4afcb02839f37104d155438112ca796e0a49675
AKJ95 <andyykp@gmail.com>
2024-08-23 10:35:08 +0100

    try gold evaluation

--- src/gold_eval_mm_docs.py
+++ src/gold_eval_mm_docs.py
@@ -172,7 +172,10 @@ if __name__ == '__main__':
                         print(embedding_tokens + pred_entity_tokens)
                         toy_vec = toks2vecs((embedding_tokens + pred_entity_tokens)[:128])
                         vectors.append(toy_vec)
-                        labels.append(0)
+                        if gold_entity_name == pred_entity_name:
+                            labels.append(1)
+                        else:
+                            labels.append(0)
 
                 pred_entities = [entry[0] for entry in sent_preds['spans'][i]['cui']]
                 gold_entity = gold_sent['spans'][i]['cui'].lstrip('UMLS:')
