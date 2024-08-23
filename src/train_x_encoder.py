170227994cff9f8929e9290ee79194a1fb57871b
AKJ95 <andyykp@gmail.com>
2024-08-23 08:19:34 +0100

    try gold evaluation

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -151,9 +151,9 @@ if __name__ == '__main__':
                 entity_example_count = 1
                 mention_count += 1
                 current_correct_prob = dev_preds[i]
-            else:
+            if dev_labels[i] == 0:
                 entity_example_count += 1
-                if dev_preds[i] > current_correct_prob:
+                if dev_preds[i] >= current_correct_prob:
                     correct_flag = False
             if i == len(dev_preds) - 1 or dev_labels[i+1] == 1:
                 if entity_example_count == 4:
