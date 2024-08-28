d5ccb225ae57b446f9c8a9c03261f3430da7bb22
AKJ95 <andyykp@gmail.com>
2024-08-28 20:43:55 +0100

    try gold evaluation

--- src/eval_mm_spans.py
+++ src/eval_mm_spans.py
@@ -132,8 +132,8 @@ if __name__ == '__main__':
     em_ner_path = 'models/ExactMatchNER/umls.2017AA.active.st21pv.nerfed_nlp_and_matcher.max3.p'
     # ngram_db_path = 'models/SimString/umls.2017AA.active.st21pv.aliases.3gram.5toks.db'
     # ngram_map_path = 'models/SimString/umls.2017AA.active.st21pv.aliases.5toks.map'
-    ngram_db_path = 'data/processed/umls.2017AA.active.st21pv.aliases.3gram.5toks.db'
-    ngram_map_path = 'data/processed/umls.2017AA.active.st21pv.aliases.5toks.map'
+    ngram_db_path = 'data/processed/umls.2024AA.active.st21pv.aliases.3gram.5toks.db'
+    ngram_map_path = 'data/processed/umls.2024AA.active.st21pv.aliases.5toks.map'
     st_vsm_path = 'models/VSMs/mm_st21pv.sts_anns.scibert_scivocab_uncased.vecs'
     # cui_vsm_path = 'models/VSMs/mm_st21pv.cuis.scibert_scivocab_uncased.vecs'
     # 1-NN Classifier
