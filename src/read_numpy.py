8f14ac995df415350fabb4bc35f975005e52568a
AKJ95 <andyykp@gmail.com>
2024-08-29 15:51:41 +0100

    try gold evaluation

--- src/read_numpy.py
+++ src/read_numpy.py
@@ -1,5 +1,11 @@
 import numpy as np
 
 
-toy_array = np.load('data/processed/x_encoder_vectors.npy')
-print(toy_array[:5, :10])
+train_labels = np.load('data/processed/x_encoder_labels.npy')
+positive_counts = np.sum(train_labels)
+positive_proportion = positive_counts / len(train_labels)
+weight_positive = 1 / positive_proportion
+print(f"Total number of labels: {len(train_labels)}")
+print(f"Positive labels: {positive_counts}")
+print(f"Positive proportion: {positive_proportion}")
+print(f"Weight for positive class: {weight_positive}")
