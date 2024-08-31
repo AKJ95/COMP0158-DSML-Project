0b9af6648e1fc2de32a324fc6cecef77692af126
AKJ95 <andyykp@gmail.com>
2024-08-31 01:58:59 +0100

    Save results of str_1nn.

--- src/train_x_encoder.py
+++ src/train_x_encoder.py
@@ -37,8 +37,8 @@ class MLP(nn.Module):
 
 
 if __name__ == '__main__':
-    x_encoder_vectors = np.load('data/processed/x_encoder_vectors.npy')
-    x_encoder_labels = np.load('data/processed/x_encoder_labels.npy')
+    x_encoder_vectors = np.load('data/processed/x_encoder_vectors_2017.npy')
+    x_encoder_labels = np.load('data/processed/x_encoder_labels_2017.npy')
     x_encoder_vectors_dev = np.load('data/processed/x_encoder_vectors_dev.npy')
     x_encoder_labels_dev = np.load('data/processed/x_encoder_labels_dev.npy')
 
