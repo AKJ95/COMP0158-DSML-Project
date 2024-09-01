289f5de9ce4e7fff8f8d98ffb054c992bf32ef62
AKJ95 <andyykp@gmail.com>
2024-09-01 10:35:58 +0100

    Save results of clf.

--- src/eval_mm_spans.py
+++ src/eval_mm_spans.py
@@ -268,5 +268,5 @@ if __name__ == '__main__':
         results = {"gold_labels": gold_labels, "pred_labels": pred_labels}
         results_str = json.dumps(results)
         # Write the string to a file
-        with open('results/str_1nn_clf.txt', 'w') as file:
+        with open('results/str_1nn_clf_avg.txt', 'w') as file:
             file.write(results_str)
