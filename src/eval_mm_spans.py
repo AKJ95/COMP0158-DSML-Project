fd4958044e45c05fab77c065558e693b227738c8
AKJ95 <andyykp@gmail.com>
2024-09-01 15:26:49 +0100

    Updated method to reconstruct spans from BIO tags.

--- src/eval_mm_spans.py
+++ src/eval_mm_spans.py
@@ -178,8 +178,8 @@ if __name__ == '__main__':
     predict_sty, require_sty = False, False
     if mm_ann == 'cui':
         print("Predicting for CUIs...")
-        medlinker.load_cui_VSM(cui_vsm_path)
-        # medlinker.load_cui_softmax_pt()
+        # medlinker.load_cui_VSM(cui_vsm_path)
+        medlinker.load_cui_softmax_pt()
         # medlinker.load_cui_clf(cui_clf_path)
         # cui_val_path = 'models/Validators/mm_st21pv.lr_clf_cui.dev.joblib'
         # medlinker.load_cui_validator(cui_val_path, validator_thresh=0.75)
