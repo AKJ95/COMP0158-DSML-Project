5c106d804e24cc913799ebc3f880be311a1a71d5
AKJ95 <andyykp@gmail.com>
2024-09-03 17:57:15 +0100

    Updated method to reconstruct spans from BIO tags.

--- src/gold_eval_mm_docs.py
+++ src/gold_eval_mm_docs.py
@@ -29,7 +29,7 @@ medner = NERComponent()
 print('Loading MedLinker ...')
 medlinker = MedLinker(medner, umls_kb)
 medlinker.load_string_matcher(ngram_db_path, ngram_map_path)
-medlinker.load_cui_softmax_pt()
+# medlinker.load_cui_softmax_pt()
 medlinker.load_cui_VSM(cui_vsm_path)
 
 
