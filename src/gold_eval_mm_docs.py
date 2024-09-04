eb529647dcfa80b2473d0a31f8fd041fd3533d7f
AKJ95 <andyykp@gmail.com>
2024-09-04 21:40:37 +0100

    Updated method to reconstruct spans from BIO tags.

--- src/gold_eval_mm_docs.py
+++ src/gold_eval_mm_docs.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
     perf_cui = {'tp': 0, 'fp': 0, 'fn': 0}
 
     logging.info('Loading MedMentions ...')
-    mm_docs = read_mm_converted('data/processed/mm_converted.train.json')
+    mm_docs = read_mm_converted('data/processed/mm_converted.dev.json')
 
     logging.info('Processing Instances ...')
     span_count = 0
@@ -207,5 +207,5 @@ if __name__ == '__main__':
     labels_np = np.array(labels)
     # print(vector_np.shape)
     # print(labels_np.shape)
-    np.save('data/processed/x_encoder_vectors_ens_train.npy', vector_np)
-    np.save('data/processed/x_encoder_labels_ens_train.npy', labels_np)
+    np.save('data/processed/x_encoder_vectors_ens_dev.npy', vector_np)
+    np.save('data/processed/x_encoder_labels_ens_dev.npy', labels_np)
