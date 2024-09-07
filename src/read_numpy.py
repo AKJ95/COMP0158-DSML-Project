104810a117b7756880262fbe63d77b383a3a1af4
AKJ95 <andyykp@gmail.com>
2024-09-07 20:44:57 +0100

    New.

--- src/read_numpy.py
+++ src/read_numpy.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-train_vectors = np.load('data/processed/x_encoder_vectors_ens_dev.npy')
-train_labels = np.load('data/processed/x_encoder_labels_ens_dev.npy')
+train_vectors = np.load('data/processed/x_encoder_vectors_ens_test.npy')
+train_labels = np.load('data/processed/x_encoder_labels_ens_test.npy')
 positive_counts = np.sum(train_labels)
 positive_proportion = positive_counts / len(train_labels)
 weight_positive = 1 / positive_proportion
@@ -10,5 +10,3 @@ print(f"Total number of labels: {len(train_labels)}")
 print(f"Positive labels: {positive_counts}")
 print(f"Positive proportion: {positive_proportion}")
 print(f"Weight for positive class: {weight_positive}")
-
-print(train_vectors[:7, :10])
